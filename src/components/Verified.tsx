0b97d350b84a24acaa105b38da7d63b5304b6cac
mathlete76 <davie.technolab@gmail.com>
2023-06-26 23:52:48 +0000

    DB Check

--- src/components/Verified.tsx
+++ src/components/Verified.tsx
@@ -1,5 +1,5 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
+import { Pool } from 'pg';
 import { FC, useCallback } from 'react';
 import { notify } from "../utils/notifications";
 import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
@@ -11,6 +11,25 @@ export const Verified: FC = () => {
 
     const DATABASE_URL = `postgresql://${process.env.NEXT_PUBLIC_PLANETSCALE_DB_USERNAME}:${process.env.NEXT_PUBLIC_PLANETSCALE_DB_PASSWORD}@${process.env.NEXT_PUBLIC_PLANETSCALE_DB_HOST}/${process.env.NEXT_PUBLIC_PLANETSCALE_DB}`;
 
+    const pool = new Pool({
+        connectionString: DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      });
+
+    const query = async (text, params) => {
+        const start = Date.now();
+        const res = await pool.query(text, params);
+        const duration = Date.now() - start;
+        console.log('executed query', { text, duration, rows: res.rowCount });
+        return res;
+      };
+
+    const getVIPByPublicKey = async (publicKey) => {
+        const res = await query('SELECT * FROM ts_vip WHERE pubkey = $1', [publicKey]);
+        return res.rows[0];
+    };
 
     const checkKYC = useCallback(async () => {
         if (!publicKey) {
@@ -20,6 +39,23 @@ export const Verified: FC = () => {
         }
 
         console.log(DATABASE_URL);
+
+        try {
+            if (publicKey) {
+                const vip = await getVIPByPublicKey(publicKey.toBase58());
+                console.log(vip);
+                if (vip) {
+                    console.log("VIP FOUND");
+                    notify({ type: 'success', message: 'VIP FOUND', description: 'VIP FOUND!' });
+                } else {
+                    console.log("VIP NOT FOUND");
+                    notify({ type: 'error', message: 'VIP NOT FOUND', description: 'VIP NOT FOUND!' });
+                }
+            }
+        } catch (error) {
+            console.error('Error checking VIP status:', error);
+        };
+
 
 }, [publicKey, connection, getUserSOLBalance]);
 
