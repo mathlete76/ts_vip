0a14d3938acc1e6f13b689c43bbf4132168296a1
mathlete76 <davie.technolab@gmail.com>
2023-06-27 00:52:57 +0000

    forgot

--- src/components/Verified.tsx
+++ src/components/Verified.tsx
@@ -2,32 +2,11 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { FC, useCallback } from 'react';
 import { notify } from "../utils/notifications";
 import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
-import serverlessMysql from 'serverless-mysql';
 
 export const Verified: FC = () => {
     const { connection } = useConnection();
     const { publicKey } = useWallet();
     const { getUserSOLBalance } = useUserSOLBalanceStore();
-
-    const db = serverlessMysql({
-        config: {
-            host: process.env.NEXT_PUBLIC_PLANETSCALE_DB_HOST,
-            database: process.env.NEXT_PUBLIC_PLANETSCALE_DB,
-            user: process.env.NEXT_PUBLIC_PLANETSCALE_DB_USERNAME,
-            password: process.env.NEXT_PUBLIC_PLANETSCALE_DB_PASSWORD,
-        }
-    });
-
-    const query = async (query, values) => {
-        const results = await db.query(query, values);
-        await db.end();
-        return results;
-      };
-
-    const getVIPByPublicKey = async (publicKey) => {
-        const res = await query('SELECT * FROM ts_vip WHERE pubkey = ?', [publicKey]);
-        return res[0];
-      };
 
     const checkKYC = useCallback(async () => {
         if (!publicKey) {
@@ -40,15 +19,6 @@ export const Verified: FC = () => {
 
         try {
             if (publicKey) {
-                const vip = await getVIPByPublicKey(publicKey.toBase58());
-                console.log(vip);
-                if (vip) {
-                    console.log("VIP FOUND");
-                    notify({ type: 'success', message: 'VIP FOUND', description: 'VIP FOUND!' });
-                } else {
-                    console.log("VIP NOT FOUND");
-                    notify({ type: 'error', message: 'VIP NOT FOUND', description: 'VIP NOT FOUND!' });
-                }
             }
         } catch (error) {
             console.error('Error checking VIP status:', error);
