dd4e742c39d4abdd244df69d3d0849c614454380
mathlete76 <davie.technolab@gmail.com>
2023-06-28 16:10:51 +0000

    display name

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -195,7 +195,7 @@ export const Landing: FC = () => {
                         <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
                         <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
                             <pre data-prefix=">">
-                                <code className="truncate">{ourWallet?.publicKey?.toBase58()}</code>
+                                <code className="truncate">{vipAccountData.username}</code>
                             </pre>
                         </div>
                     </div>
