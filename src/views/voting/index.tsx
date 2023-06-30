95d624ecd5ffe174d844c146d2884e1dba905534
mathlete76 <davie.technolab@gmail.com>
2023-06-30 22:00:49 +0000

    why is this wrong?

--- src/views/voting/index.tsx
+++ src/views/voting/index.tsx
@@ -9,6 +9,7 @@ export const VotingView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
+          
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
           Goodfellas
         </h1>
