8301913f73b27c2338949ea93407872cddd0abac
mathlete76 <davie.technolab@gmail.com>
2023-06-30 20:36:48 +0000

    changed listing to only non-members

--- src/components/Voting.tsx
+++ src/components/Voting.tsx
@@ -151,6 +151,8 @@ export const Voting: FC = () => {
         <div className="flex flex-col justify-center">
             {vipAccounts.length > 0 ? (
                 vipAccounts.map((vipAccount, index) => (
+                    <div>
+                        { vipAccount.member ? (
                     <div key={index} className="relative group">
                         <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
                         <pre data-prefix=">">
@@ -166,6 +168,7 @@ export const Voting: FC = () => {
                                         <span>Vote</span>
                                     </button>
                         </div>
+                    </div> ) : (<div></div>)}
                     </div>
                 ))
             ) : (
