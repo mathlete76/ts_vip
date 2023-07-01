ea20caa8660f319e4af0bcbb1ea990a8ad184726
mathlete76 <davie.technolab@gmail.com>
2023-07-01 02:34:18 +0000

    calm the beans on the vip account checks

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -64,7 +64,6 @@ export const Landing: FC = () => {
     const [isKYCd, setKYCstatus] = useState(null);
     const [passedKYC, setPassedKYC] = useState(null);
     const [isAdmin, setAdmin] = useState(null);
-    const [vipAccountChecked, setVipAccountChecked] = useState(false);
 
 
     const checkVIPAccount = async () => {
@@ -122,15 +121,15 @@ export const Landing: FC = () => {
             console.log('error', 'Wallet not connected!');
             return;
         }
-
+    
         let payload = {
             reference: vipAccountData.reference,
         }
-
+    
         const btoa_string = process.env.NEXT_PUBLIC_SP_API_KEY + ":" + process.env.NEXT_PUBLIC_SP_API_SECRET;
-
+    
         var token = btoa(btoa_string);
-
+    
         const response = await fetch('https://api.shuftipro.com/status', {
             method: 'post',
             headers: {
@@ -140,62 +139,62 @@ export const Landing: FC = () => {
             },
             body: JSON.stringify(payload)
         });
-
+    
         const data = await response.json();
-
+    
         console.log("KYC RESPONSE: ", data);
-
+    
         if (data.event && data.event === 'verification.accepted') {
             const provider = getProvider();
             const program = new Program(idl_object, programID, provider);
-
+    
             const [vipPda] = await PublicKey.findProgramAddressSync([
                 utils.bytes.utf8.encode(init_string),
                 provider.wallet.publicKey.toBuffer(),
             ], program.programId
             );
-
+    
             const tx = await program.methods.verify(true).accounts({
                 vip: vipPda,
                 authority: provider.wallet.publicKey,
                 systemProgram: web3.SystemProgram.programId,
             }).rpc();
-
+    
             const latestBlockHash = await program.provider.connection.getLatestBlockhash();
             const confirmation = await program.provider.connection.confirmTransaction({
                 blockhash: latestBlockHash.blockhash,
                 lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
                 signature: tx
             });
-
+    
             notify({ type: 'success', message: 'KYC Accepted', description: tx });
-
+    
             console.log("KYC ACCEPTED");
-
+    
             setKYCstatus(true);
-
+    
             setPassedKYC(true);
-
+    
             if (!confirmation.value.err) {
                 try {
                     const [membersPDA] = await PublicKey.findProgramAddressSync([
                         utils.bytes.utf8.encode("members_b"),
                     ], program.programId
                     );
-
+    
                     const tx2 = await program.methods.addMember(ourWallet.publicKey.toBase58()).accounts({
                         members: membersPDA,
                         authority: provider.wallet.publicKey,
                         systemProgram: web3.SystemProgram.programId,
                     }).rpc();
-
+    
                     const latestBlockHash2 = await program.provider.connection.getLatestBlockhash();
                     await program.provider.connection.confirmTransaction({
                         blockhash: latestBlockHash2.blockhash,
                         lastValidBlockHeight: latestBlockHash2.lastValidBlockHeight,
                         signature: tx2
                     });
-
+    
                     notify({ type: 'success', message: 'Member List Updated', description: tx2 });
                 } catch (error) {
                     notify({ type: 'error', message: 'Member List Update Failed', description: error });
@@ -203,49 +202,40 @@ export const Landing: FC = () => {
                 };
             }
         };
-
-        setVipAccountChecked(false);
     };
+    
 
-   
 
     useEffect(() => {
         if (ourWallet) {
-            if (!vipAccountChecked) {
-                checkVIPAccount();
-                setVipAccountChecked(true);
-            }
-        } else {
-            // Reset vipAccountChecked to false when the wallet is disconnected
-            setVipAccountChecked(false);
+            checkVIPAccount();
         }
-    }, [ourWallet, checkVIPAccount, vipAccountChecked]);
+    }, [ourWallet, checkVIPAccount]);
 
-
-    // useEffect(() => {
-    //     // This function checks the KYC status
-    //     const checkStatus = async () => {
-    //         if (document.visibilityState === 'visible') {
-    //             // Wait for 1 second before checking the KYC status
-    //             setTimeout(async () => {
-    //                 await checkVIPAccount();
-    //             }, 3000);
-    //         }
-    //     };
-
-    //     // Call the function once when the component mounts
-    //     checkStatus();
-
-    //     // Set up an event listener to call the function whenever the visibility state changes
-    //     document.addEventListener('visibilitychange', checkStatus);
-
-    //     // Clean up the event listener when the component unmounts
-    //     return () => {
-    //         document.removeEventListener('visibilitychange', checkStatus);
-    //     };
-    // }, [checkVIPAccount]);
-
-
+    useEffect(() => {
+        // This function checks the KYC status
+        const checkStatus = async () => {
+            if (document.visibilityState === 'visible') {
+                // Wait for 1 second before checking the KYC status
+                setTimeout(async () => {
+                    await checkVIPAccount();
+                }, 3000);
+            }
+        };
+    
+        // Call the function once when the component mounts
+        checkStatus();
+    
+        // Set up an event listener to call the function whenever the visibility state changes
+        document.addEventListener('visibilitychange', checkStatus);
+    
+        // Clean up the event listener when the component unmounts
+        return () => {
+            document.removeEventListener('visibilitychange', checkStatus);
+        };
+    }, [checkVIPAccount]);
+    
+    
 
     const [userInput, setUserInput] = useState('');
 
@@ -293,8 +283,6 @@ export const Landing: FC = () => {
             // If the account doesn't exist, set state to false
             setHasVIPAccount(false);
         }
-
-        setVipAccountChecked(false);
     };
 
 
@@ -368,8 +356,6 @@ export const Landing: FC = () => {
             console.error('Error starting KYC process:', error);
         }
 
-        setVipAccountChecked(false);
-
     };
 
     return (
@@ -418,30 +404,30 @@ export const Landing: FC = () => {
                                     </button>
                                 </div>
                             )}
-                        </div>) : (
-                        <span className="text-green-500">Verified</span>
-                    )}
+                            </div>) : (
+                                    <span className="text-green-500">Verified</span>
+                            )}
                 </div>) : (
-                <div>
-                    <div className="relative group items-center">
-                        <input
-                            className="px-8 m-2 text-black"
-                            placeholder="Username"
-                            value={userInput}
-                            onChange={(e) => setUserInput(e.target.value)}
-                        />
-                    </div>
+                    <div>
+                <div className="relative group items-center">
+                    <input
+                        className="px-8 m-2 text-black"
+                        placeholder="Username"
+                        value={userInput}
+                        onChange={(e) => setUserInput(e.target.value)}
+                    />
+                </div>
 
-                    <div className="relative group items-center">
-                        <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
+                <div className="relative group items-center">
+                    <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
                     rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-                        <button
-                            className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                            onClick={createVIPAccount}
-                        >
-                            <span>Create Account</span>
-                        </button>
-                    </div>
+                    <button
+                        className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                        onClick={createVIPAccount}
+                    >
+                        <span>Create Account</span>
+                    </button>
+                </div>
                 </div>
             )}
         </div>
