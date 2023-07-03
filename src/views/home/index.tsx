e0635c40592675caa680b1861ba21f153c905981
mathlete76 <davie.technolab@gmail.com>
2023-07-03 23:45:12 +0000

    some logo action

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -31,7 +31,11 @@ export const HomeView: FC = ({ }) => {
 
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        <img className="mx-auto" src="/gfbm.png" alt="logo" />
+        <img
+          className="mx-auto h-32 w-32 object-contain border-4 border-indigo-500 rounded-full shadow-lg"
+          src="/gfbm.png"
+          alt="logo"
+        />
         <div className="flex flex-col mt-2">
           <Landing />
         </div>
