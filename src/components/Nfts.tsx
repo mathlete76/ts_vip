0e47a635d2cd660441b42ff3c2f06674d0ef3d2c
mathlete76 <davie.technolab@gmail.com>
2023-07-03 02:08:15 +0000

    Changed PublicKey import

--- src/components/Nfts.tsx
+++ src/components/Nfts.tsx
@@ -1,71 +1,20 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { PublicKey, Keypair } from '@solana/web3.js';
-import { FC, useCallback, useEffect, useImperativeHandle, useState } from 'react';
+import { FC, useState } from 'react';
 import idl from "./ts_sol.json";
-import { Program, AnchorProvider, web3, utils, BN } from "@coral-xyz/anchor"
 import { notify } from 'utils/notifications';
-import { mintV2, mplCandyMachine, fetchCandyMachine, fetchCandyGuard } from "@metaplex-foundation/mpl-candy-machine";
+import { mintV2, mplCandyMachine, fetchCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
 import { transactionBuilder, generateSigner, publicKey, some } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
 
-
-import {
-    PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID
-} from '@metaplex-foundation/mpl-token-metadata';
-
-import { ASSOCIATED_PROGRAM_ID } from '@coral-xyz/anchor/dist/cjs/utils/token';
-
-
-
 const idl_string = JSON.stringify(idl);
 const idl_object = JSON.parse(idl_string);
-const programID = new PublicKey(idl.metadata.address);
-const init_string = "gf_a";
-
 
 export const Nfts: FC = () => {
-    const { connection } = useConnection();
+    
     const ourWallet = useWallet();
-
-    const getProvider = () => {
-        const provider = new AnchorProvider(connection, ourWallet, AnchorProvider.defaultOptions());
-        return provider;
-    };
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentImage, setCurrentImage] = useState(null);
-    const [nfts, setNfts] = useState(null);
-    const [rawNfts, setRawNfts] = useState(null);
-    const [vipAccountData, setVipAccountData] = useState(null);
-
-    // const getNFTs = async () => {
-
-    //     const metaplex = new Metaplex(connection).use(walletAdapterIdentity(ourWallet));
-
-    //     const result = await metaplex.nfts().findAllByOwner({ owner: creator });
-
-    //     setRawNfts(result);
-
-    //     const nfts = await Promise.all(result.map(async (nft) => {
-    //         const metadataResponse = await fetch(nft.uri);
-    //         const metadata = await metadataResponse.json();
-    //         return { ...nft, metadata };
-    //     }));
-
-    //     setNfts(nfts);
-
-    //     console.log("NFTs: ", nfts);
-    //     console.log("Raw NFTs: ", result);
-    // }
-
-    // useEffect(() => {
-    //     if (ourWallet?.publicKey) {
-    //         getNFTs();
-    //     }
-    // }, [ourWallet]);
-
+    
     const mintNFT = async () => {
 
         if (ourWallet?.publicKey) {
